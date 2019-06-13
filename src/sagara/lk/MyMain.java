87f477dba4d8f9610a4448afa56415e4e2643ceb
Sagara <sagarajlk@gmail.com>
2019-06-13 09:53:10 +0530

    Sagara 1 done

---
+++ src/sagara/lk/MyMain.java
@@ -0,0 +1,19 @@
+package sagara.lk;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+
+		String[] names = {"Sagara", "Kamal", "Saman"};
+		
+		//Print Greeting
+		for (int i = 0; i < names.length; i++) {
+			
+			System.out.format("Hello, %s!\n", names[i]);
+			
+		}
+		
+
+	}
+
+}
